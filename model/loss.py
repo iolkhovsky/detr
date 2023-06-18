cf30fa9b8da4513b6e224666b75b7437880ad9dc
Igor Olkhovskiy <i.v.olkhovsky@gmail.com>
2023-06-18 11:36:10 +0300

    Add plug for loss

---
+++ model/loss.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+
+class BipartiteMatchingLoss(nn.Module):
+    def __init__(self):
+        super(BipartiteMatchingLoss, self).__init__()
+
+    def forward(self, pred, target):
+        pass
