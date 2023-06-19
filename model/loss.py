3b720c9946f4a41909c849081120a777d5502147
Igor Olkhovskiy <i.v.olkhovsky@gmail.com>
2023-06-19 20:12:19 +0300

    Add plug for loss

--- model/loss.py
+++ model/loss.py
@@ -1,10 +1,34 @@
 import torch
 import torch.nn as nn
 
+from model.matcher import HungarianMatcher
+
+
+class RegressionLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(prediction, target):
+        pass
+
+
+class ClassificationLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(prediction, target):
+        pass
 
 class BipartiteMatchingLoss(nn.Module):
     def __init__(self):
         super(BipartiteMatchingLoss, self).__init__()
+        self._matcher = HungarianMatcher()
+        self._clf = ClassificationLoss()
+        self._regr = RegressionLoss()
 
     def forward(self, prediction, targets):
-        pass
+        matching = self._matcher(
+            outputs=prediction,
+            targets=targets,
+        )
+        print(matching)
